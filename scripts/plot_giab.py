e9d4b6af5453afa0fd791f4b1a74215c30906ef6
Luca Denti <ldenti@proton.me>
2025-08-15 13:35:44 +0200

    Update giab plot

--- scripts/plot_giab.py
+++ scripts/plot_giab.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import argparse
 import glob
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,7 +14,7 @@ TOOLS = [
     "sawfish",
     "severus-w4",
     "sniffles",
-    # "svisionpro-w4",
+    "svisionpro-w4",
     "SVDSS-w4",
     "SVDSS2ht-q0.98-w4",
 ]
@@ -35,7 +36,7 @@ x_order = [
     "sawfish",
     "severus",
     "sniffles",
-    # "svisionpro",
+    "SVision-pro",
 ]
 
 
@@ -53,6 +54,8 @@ def parse_csv(fn, refseq, tools=TOOLS):
             tool = "cuteSV"
         elif tool == "SVDSS2ht":
             tool = "SVDSS2"
+        elif tool == "svisionpro":
+            tool = "SVision-pro"
         if float(F1) == 0.0:
             continue
         data.append([refseq, tool, float(P), float(R), float(F1)])
@@ -64,17 +67,21 @@ def parse_csv(fn, refseq, tools=TOOLS):
 
 def main():
     sns.set(font_scale=0.75)
-    t2t_fn = sys.argv[1]
-    hg38_fn = sys.argv[2]
-    hg19_fn = sys.argv[3]
-    hg19_v06_fn = sys.argv[4]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--bed", action='store_true')
+    parser.add_argument("t2t")
+    parser.add_argument("hg38")
+    parser.add_argument("hg19")
+    parser.add_argument("hg19old")
+    args = parser.parse_args()
 
     fig, axes = plt.subplots(2, 4, figsize=(10, 5))
 
     # GIAB v0.6
     col = 0
     ref = "GRCh37"
-    data = parse_csv(hg19_v06_fn, "GRCh37", TOOLS)
+    data = parse_csv(args.hg19old, "GRCh37", TOOLS)
     df = pd.DataFrame(data, columns=["Ref", "Tool", "P", "R", "F1", "rank"])
     sns.scatterplot(
         data=df[(df["Ref"] == ref) & (df["F1"] > 0)],
@@ -94,18 +101,16 @@ def main():
         ax=axes[1][col],
         hue="Tool",
     )
-    axes[0][col].set_title("(a)\n" + ref + " (GIAB v0.6, Tier 1)")
-    axes[0][col].set_xlim([75, 100])
-    axes[0][col].set_ylim([45, 100])
+    axes[0][col].set_title("(a)\n" + ref + " (GIAB v0.6" + (")" if not args.bed else ", Tier 1)"))
     axes[1][col].set_ylim([0, 100])
     for i, container in enumerate(axes[1][col].containers):
         axes[1][col].bar_label(container, labels=df[df["Tool"] == x_order[i]]["rank"])
     axes[1][col].tick_params(axis="x", labelrotation=90)
 
     data = []
-    data += parse_csv(t2t_fn, "T2T")
-    data += parse_csv(hg38_fn, "GRCh38")
-    data += parse_csv(hg19_fn, "GRCh37")
+    data += parse_csv(args.t2t, "T2T")
+    data += parse_csv(args.hg38, "GRCh38")
+    data += parse_csv(args.hg19, "GRCh37")
 
     df = pd.DataFrame(data, columns=["Ref", "Tool", "P", "R", "F1", "rank"])
     print(df)
@@ -132,10 +137,7 @@ def main():
             # alpha=0.75,
         )
 
-        axes[0][col].set_title(f"({('abcd'[col])})\n" + ref + " (GIAB v1.1, w/ BED)")
-        axes[0][col].set_xlim([75, 100])
-        axes[0][col].set_ylim([45, 100])
-
+        axes[0][col].set_title(f"({('abcd'[col])})\n" + ref + " (GIAB v1.1" + (")" if not args.bed else ", w/ BED)"))
         axes[1][col].set_ylim([0, 100])
 
         for i, container in enumerate(axes[1][col].containers):
@@ -150,6 +152,11 @@ def main():
             axes[0][col].set_ylabel("")
             axes[1][col].set_ylabel("")
 
+    xlim = 40 # 80 if args.bed else 40
+    ylim = 30 # 40 if args.bed else 30
+    for col in range(4):
+        axes[0][col].set_xlim([xlim, 100])
+        axes[0][col].set_ylim([ylim, 100])
     plt.tight_layout()
     plt.show()
     # plt.savefig(csv_fn + ".png", dpi=300)
